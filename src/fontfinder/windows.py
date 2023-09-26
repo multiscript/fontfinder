80cfb3b5827bd48388c31ba7a97998852cf8dcc3
jamesbcd <50201699+jamesbcd@users.noreply.github.com>
2023-09-26 13:38:13 +1000

    Actually retrieve font list from DirectWrite

--- src/fontfinder/windows.py
+++ src/fontfinder/windows.py
@@ -4,24 +4,54 @@
 
 from comtypes import COMError, GUID, HRESULT, IUnknown, STDMETHOD, WINFUNCTYPE
 import ctypes
-from ctypes import byref, create_unicode_buffer, POINTER, windll, wintypes
+from ctypes import byref, POINTER, wintypes
 from enum import IntEnum
 import platform
 from sys import getwindowsversion
-from typing import Set
 
 from fontfinder.all_platforms import CTypesLibrary
 
 def all_installed_families():
     if platform.system() != "Windows":
-        raise Exception("fontfinder.mac.all_installed_families() should only be called under macOS")
+        raise Exception("fontfinder.windows.all_installed_families() should only be called under Windows")
     
     dw = DirectWriteLibrary()
     dw_factory = POINTER(IDWriteFactory)()
     dw.DWriteCreateFactory(dw.DWRITE_FACTORY_TYPE_ISOLATED, IDWriteFactory._iid_, byref(dw_factory))
-    print(dw_factory)
 
-    return []
+    fonts = POINTER(IDWriteFontCollection)()
+    dw_factory.GetSystemFontCollection(byref(fonts), False)
+
+    # Use a dict as an ordered set
+    family_names = {}
+    for i in range(fonts.GetFontFamilyCount()):
+        font_family = POINTER(IDWriteFontFamily)()
+        fonts.GetFontFamily(i, byref(font_family))
+
+        family_name_strings = POINTER(IDWriteLocalizedStrings)()
+        font_family.GetFamilyNames(byref(family_name_strings))
+
+        name_index = 0
+        name_len = ctypes.c_uint32()
+        family_name_strings.GetStringLength(name_index, byref(name_len))
+
+        name_buffer = ctypes.create_unicode_buffer(name_len.value+1)
+        family_name_strings.GetString(name_index, name_buffer, name_len.value+1)
+        family_names[name_buffer.value] = 1
+
+    return list(family_names.keys())
+
+
+class IDWriteLocalizedStrings(IUnknown):
+    _iid_ = GUID("{08256209-099a-4b34-b86d-c22b110e7771}")
+    _methods_ = [
+        STDMETHOD(ctypes.c_uint32, "GetCount"),
+        STDMETHOD(HRESULT, "FindLocaleName"),
+        STDMETHOD(HRESULT, "GetLocaleNameLength"),
+        STDMETHOD(HRESULT, "GetLocaleName"),
+        STDMETHOD(HRESULT, "GetStringLength", [ctypes.c_uint32, POINTER(ctypes.c_uint32)]),
+        STDMETHOD(HRESULT, "GetString", [ctypes.c_uint32, POINTER(wintypes.WCHAR), ctypes.c_uint32])
+    ]
 
 
 class IDWriteFontList(IUnknown):
@@ -29,7 +59,7 @@ class IDWriteFontList(IUnknown):
     _iid_ = GUID("{1a0d8438-1d97-4ec1-aef9-a2fb86ed6acb}")
     _methods_ = [
         STDMETHOD(None, "GetFontCollection"),       # Not implemented here
-        STDMETHOD(wintypes.UINT, "GetFontCount"),   # Not implemented here
+        STDMETHOD(wintypes.UINT, "GetFontCount"),
         STDMETHOD(HRESULT, "GetFont"),              # Not implemented here
     ]
 
@@ -38,7 +68,7 @@ class IDWriteFontFamily(IDWriteFontList):
     # https://learn.microsoft.com/en-us/windows/win32/api/dwrite/nn-dwrite-idwritefontfamily
     _iid_ = GUID("{da20d8ef-812a-4c43-9802-62ec4abd7add}")
     _methods_ = [
-        STDMETHOD(None, "GetFamilyNames"),          # Not implemented here
+        STDMETHOD(None, "GetFamilyNames", [POINTER(POINTER(IDWriteLocalizedStrings))]),
         STDMETHOD(None, "GetFirstMatchingFont"),    # Not implemented here
         STDMETHOD(None, "GetMatchingFonts"),        # Not implemented here
     ]
